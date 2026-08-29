fcaf0d8fa6447a16ca5046282413bf89d563cd16
StivenHuayta <a20240764@pucp.edu.pe>
2026-08-29 14:47:23 -0500

    Add files via upload

---
+++ Cita.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Cita {
+	private int id;
+	private Veterinario veterinario;
+	private Mascota mascota;
+	private LocalDateTime fechaHoraCita;
+	private Estado estado;
+}
---
+++ Estado.java
@@ -0,0 +1,5 @@
+public enum Estado {
+	CONFIRMADA,
+	CANCELADA,
+	ATENDIDA
+}
