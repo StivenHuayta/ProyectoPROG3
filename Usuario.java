738cd3fb88b34758bb032ffb376ae8fd6e9594d5
StivenHuayta <a20240764@pucp.edu.pe>
2026-08-29 19:08:01 -0500

    Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -3,7 +3,6 @@ public class Usuario {
 	private String dni;
 	private String nombres;
 	private String apellidos;
-	private Sexo sexo;
 	private String email;
 	private String passwordHash;
 	private String telefono;
