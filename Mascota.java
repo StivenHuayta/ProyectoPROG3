c6e46bdeaeb093fab50eef7c691e86aae556a134
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 20:10:31 -0500

    Update Mascota.java

--- Mascota.java
+++ Mascota.java
@@ -1,10 +1,22 @@
 import java.time.LocalDate;
 
 public class Mascota {
-	private int id;
-	private int clienteId;
-	private String nombre;
-	private Especie especie;
-	private LocalDate fechaNacimiento;
-	private Sexo sexo;
+    private int id;
+    private int clienteId;
+    private String nombre;
+    private Especie especie;
+    private LocalDate fechaNacimiento;
+    private Sexo sexo;
+
+    public Mascota() {}
+
+    public Mascota(int id, int clienteId, String nombre, Especie especie, 
+                   LocalDate fechaNacimiento, Sexo sexo) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+    }
 }
