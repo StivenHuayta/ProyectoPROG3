399e5b0bf9f679762fa4bbd49ad23df8459ddcac
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-01 23:07:10 -0500

    Update Atencion.java

--- Atencion.java
+++ Atencion.java
@@ -5,23 +5,18 @@ import java.util.List;
 
 public class Atencion {
     private int id;
-    private Cita cita;
-    private Mascota mascota;         // Obligatorio: a quién se atendió
-    private Empleado atendidoPor;    // Obligatorio: quién lo atendió (Veterinario o Estilista)
+    private Cita cita;   
     
-    private LocalDateTime fechaHora; // Momento exacto de la atención
-    private int idSede;
-    private int idServicio;
+    private Empleado atendidoPor; // Quién lo atendió realmente
+    private LocalDateTime fechaHora;
+
+    
     // Parámetros físicos tomados en el momento
     private double pesoActual;
-    private String temperatura;      // Opcional para estética, vital para clínica
+    private String temperatura;     
     
-    // Notas y resultados
     private String motivoUObservaciones; // Sirve para corte/baño o síntomas
     private String diagnostico;          // Null si fue baño
     private String recetaTratamiento;    // Null si fue corte
-    
-    // Servicios ejecutados y liquidación económica
-    private List<Servicio> serviciosRealizados = new ArrayList<>();
     private BigDecimal montoTotal;
 }
