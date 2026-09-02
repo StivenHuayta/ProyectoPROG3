f21f276ae2a46380a86cac8d3323f53b4343722a
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 20:09:29 -0500

    Update Servicio.java

--- Servicio.java
+++ Servicio.java
@@ -1,7 +1,10 @@
+import java.math.BigDecimal;
+
 public class Servicio {
     private int id;
-    private String nombre;             // Ej: "Consulta médica", "Baño medicado"
-    private double precioBase;
-    private TipoServicio tipoServicio; // Enum: MEDICO, ESTETICO
+    private String nombre;             // Ej: "Consulta general", "Baño medicado"
+    private int duracionMinutos;       // Indispensable para agendar
+    private BigDecimal precioBase;     // Estandarizado con BigDecimal
+    private TipoServicio tipoServicio; 
     private boolean activo;
 }
