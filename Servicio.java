50a998e084baaeb7dac5e5fa70979cd2c72c70c5
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 19:49:44 -0500

    Create Servicio.java

---
+++ Servicio.java
@@ -0,0 +1,7 @@
+public class Servicio {
+    private int id;
+    private String nombre;             // Ej: "Consulta médica", "Baño medicado"
+    private double precioBase;
+    private TipoServicio tipoServicio; // Enum: MEDICO, ESTETICO
+    private boolean activo;
+}
