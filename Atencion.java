cb6370b66ef87471c9d842573627ad5ec07cd7e7
jfhuanca <zekrom174@gmail.com>
2026-09-01 22:39:02 -0500

    Update Atencion.java

--- Atencion.java
+++ Atencion.java
@@ -8,8 +8,10 @@ public class Atencion {
     private Cita cita;
     private Mascota mascota;         // Obligatorio: a quién se atendió
     private Empleado atendidoPor;    // Obligatorio: quién lo atendió (Veterinario o Estilista)
-    private LocalDateTime fechaHora; // Momento exacto de la atención
     
+    private LocalDateTime fechaHora; // Momento exacto de la atención
+    private int idSede;
+    private int idServicio;
     // Parámetros físicos tomados en el momento
     private double pesoActual;
     private String temperatura;      // Opcional para estética, vital para clínica
