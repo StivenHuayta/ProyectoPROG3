74067eec61aa0850c618194d157e90cef3e704b6
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 19:46:43 -0500

    fas

--- Cita.java
+++ Cita.java
@@ -2,6 +2,7 @@ import java.time.LocalDateTime;
 
 public class Cita {
 	private int id;
+	private int sedeId;
 	private Empleado empleado;
 	private Mascota mascota;
 	private LocalDateTime fechaHoraCita;
