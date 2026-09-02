dd851fd148654d612518c6eee2b1e21fc3b50e8c
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-01 23:14:11 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,23 +1,13 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Cliente extends Usuario {
-	protected String direccion;
-    protected List<Mascota> mascotas;
+public class Cliente {
+	private int idCliente; 
+	private Usuario usuario; 
+	private String direccion;
+	private String contactoDeEmergencia; 
+    
+	
 
-	public Cliente() {
-		super();
-		this.mascotas = new ArrayList<>();
-	}
-
-	public Cliente(int id, String dni, String nombres, String apellidos, 
-                   String email, String passwordHash, String telefono, String direccion) {
-        super(id, dni, nombres, apellidos, email, passwordHash, telefono);
-        this.direccion = direccion;
-        this.mascotas = new ArrayList<>();
-    }
-
-	public void registrarMascota(Mascota mascota) {
-		this.mascotas.add(mascota);
-	}
+	
 }
