5e68206a45ec3a16ed0abfcfe818e782d0f1e358
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 19:08:26 -0500

    uwuwuw

---
+++ Empleado.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Empleado {
+	private String numeroColegiatura;
+	private boolean activo;
+	private List<RolEmpleado> roles;
+
+	public Empleado() {
+		super();
+		this.activo = true;
+		this.roles = new ArrayList<>();
+	}
+
+	public Empleado(int id, String dni, String nombres, String apellidos, 
+                    String email, String passwordHash, String telefono, String direccion, 
+                    String numeroColegiatura) {
+        super(id, dni, nombres, apellidos, email, passwordHash, telefono, direccion);
+        this.numeroColegiatura = numeroColegiatura;
+        this.activo = true;
+        this.roles = new ArrayList<>();
+    }
+}
--- Veterinario.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class Empleado {
-	private int id;
-	private int usuarioId;
-	private boolean activo;
-	private List<Rol> roles;//aniadido de roles
-}
