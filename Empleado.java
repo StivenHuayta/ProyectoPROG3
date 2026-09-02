79554df121a7774438cfbb119cb463f6401ea48e
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 20:34:33 -0500

    asdfvhsdfv

--- Empleado.java
+++ Empleado.java
@@ -1,33 +1,40 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Empleado extends Cliente {
-	private String numeroColegiatura;
-	private boolean activo;
-	private List<RolEmpleado> roles;
+    private String numeroColegiatura;
+    private boolean activo;
+    private List<RolEmpleado> roles;
+    private List<Horario> horarios; // Turnos base semanales
 
-	public Empleado() {
-		super();
-		this.activo = true;
-		this.roles = new ArrayList<>();
-	}
+    public Empleado() {
+        super();
+        this.activo = true;
+        this.roles = new ArrayList<>();
+        this.horarios = new ArrayList<>();
+    }
 
-	public Empleado(int id, String dni, String nombres, String apellidos, 
+    public Empleado(int id, String dni, String nombres, String apellidos, 
                     String email, String passwordHash, String telefono, String direccion, 
                     String numeroColegiatura) {
         super(id, dni, nombres, apellidos, email, passwordHash, telefono, direccion);
         this.numeroColegiatura = numeroColegiatura;
         this.activo = true;
         this.roles = new ArrayList<>();
+        this.horarios = new ArrayList<>();
     }
 
-	public void agregarRol(RolEmpleado rol) {
-	    if (!this.roles.contains(rol)) {
-	        this.roles.add(rol);
-	    }
-	}
-	
-	public boolean tieneRol(RolEmpleado rol) {
-	    return this.roles.contains(rol);
-	}
+    public void agregarRol(RolEmpleado rol) {
+        if (!this.roles.contains(rol)) {
+            this.roles.add(rol);
+        }
+    }
+
+    public boolean tieneRol(RolEmpleado rol) {
+        return this.roles.contains(rol);
+    }
+
+    public void agregarHorario(Horario horario) {
+        this.horarios.add(horario);
+    }
 }
