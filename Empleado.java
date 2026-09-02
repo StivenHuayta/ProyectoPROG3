bfe07dfe1cb1d963d316f444489aef50ef6adcd4
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-01 23:16:47 -0500

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Empleado extends Cliente {
+public class Empleado{
     private String numeroColegiatura;
     private boolean activo;
     private List<RolEmpleado> roles;
