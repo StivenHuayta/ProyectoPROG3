66d1a9c1866631e610859b733670acd1c01d016b
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-01 23:38:00 -0500

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -8,35 +8,5 @@ public class Empleado{
     //chau horario
     private Sede sede;
 
-    public Empleado() {
-        super();
-        this.activo = true;
-        this.roles = new ArrayList<>();
-
-    }
-
-    public Empleado(int id, String dni, String nombres, String apellidos, 
-                    String email, String passwordHash, String telefono, String direccion, 
-                    String numeroColegiatura,Sede sede) {
-        super(id, dni, nombres, apellidos, email, passwordHash, telefono, direccion);
-        this.numeroColegiatura = numeroColegiatura;
-        this.sede = sede;
-        this.activo = true;
-        this.roles = new ArrayList<>();
-
-    }
-
-    public void agregarRol(RolEmpleado rol) {
-        if (!this.roles.contains(rol)) {
-            this.roles.add(rol);
-        }
-    }
-
-    public boolean tieneRol(RolEmpleado rol) {
-        return this.roles.contains(rol);
-    }
-
-    public void agregarHorario(Horario horario) {
-        this.horarios.add(horario);
-    }
+    
 }
