2d19ed9ba20da9273050a0aab3a3fbb79a23b019
StivenHuayta <a20240764@pucp.edu.pe>
2026-09-01 20:08:22 -0500

    Update Cita.java

--- Cita.java
+++ Cita.java
@@ -5,6 +5,7 @@ public class Cita {
 	private int sedeId;
 	private Empleado empleado;
 	private Mascota mascota;
+	private Servicio servicio;
 	private LocalDateTime fechaHoraCita;
 	private Estado estado;
 }
