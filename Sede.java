bae6da47a61b450edd17b6ea3a4158e43fa1f4ad
jfhuanca <zekrom174@gmail.com>
2026-09-01 22:05:49 -0500

    Sede.java

---
+++ Sede.java
@@ -0,0 +1,16 @@
+public class Sede{
+  private int idSede;
+  private String nombre;
+  private String direccion;
+  private String telefono;
+  private boolean activo; 
+
+  public Sede(int idSede,String nombre,String direccion,String telefono,boolean activo){
+    this.idSede = idSede;
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.telefono = telefono;
+    this.activo = activo;
+  }
+  
+}
