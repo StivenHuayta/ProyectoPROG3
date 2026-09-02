0a2176f24419b4d513364c99b7e939122e53208c
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-01 22:37:25 -0500

    Update HistorialMascotaDTO.java

--- ClasesDTO/HistorialMascotaDTO.java
+++ ClasesDTO/HistorialMascotaDTO.java
@@ -7,7 +7,7 @@ private class HistorialMascotaDTO{
 	private String edad;
 	private String nombreOwner;
 	private String telefonoOwner;
-	private String fechaRegistro;
+	private LocalDateTime fechaRegistro;
 	private List<VisitaClinicaDTO> atencionesMedicas;
 	
 	
