0d8b9268c10294efd08c2feedc5f93f17d831686
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-01 23:18:52 -0500

    Update Mascota.java

--- Mascota.java
+++ Mascota.java
@@ -7,18 +7,20 @@ public class Mascota {
     private Especie especie;
     private LocalDate fechaNacimiento;
     private Sexo sexo;
+    private double peso;
     private LocalDate fechaRegistro;
 
     public Mascota() {}
 
     public Mascota(int id, Cliente cliente, String nombre, Especie especie, 
-                   LocalDate fechaNacimiento, Sexo sexo,LocalDate fechaRegistro) {
+                   LocalDate fechaNacimiento, Sexo sexo,double peso,LocalDate fechaRegistro) {
         this.id = id;
         this.cliente = cliente;
         this.nombre = nombre;
         this.especie = especie;
         this.fechaNacimiento = fechaNacimiento;
         this.sexo = sexo;
+        this.peso = peso;
         this.fechaRegistro = fechaRegistro;
     }
 }
