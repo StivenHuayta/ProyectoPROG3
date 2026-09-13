33a10d22b982fb2d8a2189ccb31b04d87a726f91
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:19:37 -0500

    Add constructors and getters/setters to Cliente class

--- Cliente.java
+++ Cliente.java
@@ -6,8 +6,46 @@ public class Cliente {
 	private Usuario usuario; 
 	private String direccion;
 	private String contactoDeEmergencia; 
-    
-	
 
-	
+	public Cliente() {}
+
+	public Cliente(int idCliente, Usuario usuario, String direccion, String contactoDeEmergencia) {
+		this.idCliente = idCliente;
+		this.usuario = usuario;
+		this.direccion = direccion;
+		this.contactoDeEmergencia = contactoDeEmergencia;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getContactoDeEmergencia() {
+		return contactoDeEmergencia;
+	}
+
+	public void setContactoDeEmergencia(String contactoDeEmergencia) {
+		this.contactoDeEmergencia = contactoDeEmergencia;
+	}
+
 }
