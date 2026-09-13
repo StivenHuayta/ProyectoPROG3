6ae17f6cae19d98dd26113b738a96cd6c8e49e4c
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:23:43 -0500

    Update Servicio.java

--- Servicio.java
+++ Servicio.java
@@ -7,4 +7,64 @@ public class Servicio {
     private BigDecimal precioBase;     
     private TipoServicio tipoServicio; 
     private boolean activo;
+
+    public Servicio() {}
+
+    public Servicio(int id, String nombre, int duracionMinutos, BigDecimal precioBase, TipoServicio tipoServicio, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.duracionMinutos = duracionMinutos;
+        this.precioBase = precioBase;
+        this.tipoServicio = tipoServicio;
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(int duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public BigDecimal getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(BigDecimal precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public TipoServicio getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(TipoServicio tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
 }
