03e1592bd409c19943de951a8afaea3d5e7fc091
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:32:05 -0500

    Add constructor and getters/setters to ReporteGeneralCitasDTO

--- ClasesDTO/ReporteGeneralCitasDTO.java
+++ ClasesDTO/ReporteGeneralCitasDTO.java
@@ -6,8 +6,57 @@ public class ReporteGeneralCitasDTO {
     private int totalCitasProgramadas;
     private int totalCitasCompletadas;
     private int totalCitasCanceladas;
-    
     private List<CitaDetalleDTO> detalleCitas;
 
   
+    public ReporteGeneralCitasDTO() {}
+
+    public ReporteGeneralCitasDTO(String rangoFechasEvaluado, int totalCitasProgramadas, int totalCitasCompletadas, int totalCitasCanceladas, List<CitaDetalleDTO> detalleCitas) {
+        this.rangoFechasEvaluado = rangoFechasEvaluado;
+        this.totalCitasProgramadas = totalCitasProgramadas;
+        this.totalCitasCompletadas = totalCitasCompletadas;
+        this.totalCitasCanceladas = totalCitasCanceladas;
+        this.detalleCitas = detalleCitas;
+    }
+
+    public String getRangoFechasEvaluado() {
+        return rangoFechasEvaluado;
+    }
+
+    public void setRangoFechasEvaluado(String rangoFechasEvaluado) {
+        this.rangoFechasEvaluado = rangoFechasEvaluado;
+    }
+
+    public int getTotalCitasProgramadas() {
+        return totalCitasProgramadas;
+    }
+
+    public void setTotalCitasProgramadas(int totalCitasProgramadas) {
+        this.totalCitasProgramadas = totalCitasProgramadas;
+    }
+
+    public int getTotalCitasCompletadas() {
+        return totalCitasCompletadas;
+    }
+
+    public void setTotalCitasCompletadas(int totalCitasCompletadas) {
+        this.totalCitasCompletadas = totalCitasCompletadas;
+    }
+
+    public int getTotalCitasCanceladas() {
+        return totalCitasCanceladas;
+    }
+
+    public void setTotalCitasCanceladas(int totalCitasCanceladas) {
+        this.totalCitasCanceladas = totalCitasCanceladas;
+    }
+
+    public List<CitaDetalleDTO> getDetalleCitas() {
+        return detalleCitas;
+    }
+
+    public void setDetalleCitas(List<CitaDetalleDTO> detalleCitas) {
+        this.detalleCitas = detalleCitas;
+    }
+
 }
