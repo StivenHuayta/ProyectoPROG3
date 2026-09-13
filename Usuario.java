477489d5b8f669dc1298b1abb27e28df7dd29040
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:24:15 -0500

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -8,6 +8,7 @@ public class Usuario {
     protected String passwordHash;
     protected String telefono;
     protected boolean estado;
+   
     protected Usuario() {};
     
     protected Usuario(int id, TipoRol rol,String dni, String nombres, String apellidos, 
@@ -22,4 +23,77 @@ public class Usuario {
         this.telefono = telefono;
         this.estado = estado;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TipoRol getRol() {
+        return rol;
+    }
+
+    public void setRol(TipoRol rol) {
+        this.rol = rol;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
 }
