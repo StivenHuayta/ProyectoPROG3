9f53eeaf6d6af5f3840b2fa02ded662483ac3f85
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:31:36 -0500

    Refactor HistorialMascotaDTO to public with constructors

--- ClasesDTO/HistorialMascotaDTO.java
+++ ClasesDTO/HistorialMascotaDTO.java
@@ -1,7 +1,8 @@
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ArrayList;
 
-private class HistorialMascotaDTO{
+public class HistorialMascotaDTO{
 	private String nombreMascota;
 	private String especie;
 	private String edad;
@@ -10,7 +11,72 @@ private class HistorialMascotaDTO{
 	private LocalDateTime fechaRegistro;
 	private List<VisitaClinicaDTO> atencionesMedicas;
 	
-	
-	
-	
+	public HistorialMascotaDTO() {}
+
+	public HistorialMascotaDTO(String nombreMascota, String especie, String edad, String nombreOwner, String telefonoOwner, LocalDateTime fechaRegistro, List<VisitaClinicaDTO> atencionesMedicas) {
+		this.nombreMascota = nombreMascota;
+		this.especie = especie;
+		this.edad = edad;
+		this.nombreOwner = nombreOwner;
+		this.telefonoOwner = telefonoOwner;
+		this.fechaRegistro = fechaRegistro;
+		this.atencionesMedicas = atencionesMedicas;
+	}
+
+	public String getNombreMascota() {
+		return nombreMascota;
+	}
+
+	public void setNombreMascota(String nombreMascota) {
+		this.nombreMascota = nombreMascota;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public String getEdad() {
+		return edad;
+	}
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+
+	public String getNombreOwner() {
+		return nombreOwner;
+	}
+
+	public void setNombreOwner(String nombreOwner) {
+		this.nombreOwner = nombreOwner;
+	}
+
+	public String getTelefonoOwner() {
+		return telefonoOwner;
+	}
+
+	public void setTelefonoOwner(String telefonoOwner) {
+		this.telefonoOwner = telefonoOwner;
+	}
+
+	public LocalDateTime getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(LocalDateTime fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+	public List<VisitaClinicaDTO> getAtencionesMedicas() {
+		return atencionesMedicas;
+	}
+
+	public void setAtencionesMedicas(List<VisitaClinicaDTO> atencionesMedicas) {
+		this.atencionesMedicas = atencionesMedicas;
+	}
+
 }
