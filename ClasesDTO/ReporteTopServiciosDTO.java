6571a35f37f0f7b688612639a31caf3a295ad64d
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:32:23 -0500

    Refactor ReporteTopServiciosDTO to public class
    Added constructors and getters/setters for properties.

--- ClasesDTO/ReporteTopServiciosDTO.java
+++ ClasesDTO/ReporteTopServiciosDTO.java
@@ -1,7 +1,8 @@
+import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
-private class ReporteTopServiciosDTO{
+public class ReporteTopServiciosDTO{
 	
 	private String periodoEvaluado; 
 	private Date fechaGeneracion; 
@@ -9,6 +10,45 @@ private class ReporteTopServiciosDTO{
 	private List<ServicioEstadisticaDTO> topServicios;
 	
 	
-	
-	
+	public ReporteTopServiciosDTO() {}
+
+	public ReporteTopServiciosDTO(String periodoEvaluado, Date fechaGeneracion, double ingresosTotalesDelPeriodo, List<ServicioEstadisticaDTO> topServicios) {
+		this.periodoEvaluado = periodoEvaluado;
+		this.fechaGeneracion = fechaGeneracion;
+		this.ingresosTotalesDelPeriodo = ingresosTotalesDelPeriodo;
+		this.topServicios = topServicios;
+	}
+
+	public String getPeriodoEvaluado() {
+		return periodoEvaluado;
+	}
+
+	public void setPeriodoEvaluado(String periodoEvaluado) {
+		this.periodoEvaluado = periodoEvaluado;
+	}
+
+	public Date getFechaGeneracion() {
+		return fechaGeneracion;
+	}
+
+	public void setFechaGeneracion(Date fechaGeneracion) {
+		this.fechaGeneracion = fechaGeneracion;
+	}
+
+	public double getIngresosTotalesDelPeriodo() {
+		return ingresosTotalesDelPeriodo;
+	}
+
+	public void setIngresosTotalesDelPeriodo(double ingresosTotalesDelPeriodo) {
+		this.ingresosTotalesDelPeriodo = ingresosTotalesDelPeriodo;
+	}
+
+	public List<ServicioEstadisticaDTO> getTopServicios() {
+		return topServicios;
+	}
+
+	public void setTopServicios(List<ServicioEstadisticaDTO> topServicios) {
+		this.topServicios = topServicios;
+	}
+
 }
