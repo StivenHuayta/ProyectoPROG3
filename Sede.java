feac2388bdded11225919dd3f9a6149518f7d369
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:23:06 -0500

    Update Sede.java

--- Sede.java
+++ Sede.java
@@ -13,4 +13,45 @@ public class Sede{
     this.activo = activo;
   }
   
+
+  public int getIdSede() {
+    return idSede;
+  }
+
+  public void setIdSede(int idSede) {
+    this.idSede = idSede;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getDireccion() {
+    return direccion;
+  }
+
+  public void setDireccion(String direccion) {
+    this.direccion = direccion;
+  }
+
+  public String getTelefono() {
+    return telefono;
+  }
+
+  public void setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
+
+  public boolean isActivo() {
+    return activo;
+  }
+
+  public void setActivo(boolean activo) {
+    this.activo = activo;
+  }
+
 }
