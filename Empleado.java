de1b955e1321f49626b0e42ba48129df7a490191
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:20:02 -0500

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -9,4 +9,45 @@ public class Empleado{
     private Sede sede;
 
     
+    public Empleado() {}
+
+    public Empleado(String numeroColegiatura, boolean activo, List<RolEmpleado> roles, Sede sede) {
+        this.numeroColegiatura = numeroColegiatura;
+        this.activo = activo;
+        this.roles = roles;
+        this.sede = sede;
+    }
+
+    public String getNumeroColegiatura() {
+        return numeroColegiatura;
+    }
+
+    public void setNumeroColegiatura(String numeroColegiatura) {
+        this.numeroColegiatura = numeroColegiatura;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public List<RolEmpleado> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RolEmpleado> roles) {
+        this.roles = roles;
+    }
+
+    public Sede getSede() {
+        return sede;
+    }
+
+    public void setSede(Sede sede) {
+        this.sede = sede;
+    }
+
 }
