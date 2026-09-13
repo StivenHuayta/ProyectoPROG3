caebf5f0ad29f5891b8fc407cbecc5b7af9b7fc3
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-13 18:32:44 -0500

    Refactor ServicioEstadisticaDTO to public class

--- ClasesDTO/ServicioEstadisticaDTO.java
+++ ClasesDTO/ServicioEstadisticaDTO.java
@@ -1,9 +1,41 @@
-private class ServicioEstadisticaDTO{
+public class ServicioEstadisticaDTO{
 	
 	private String nombreServicio;
 	private int cantidadSolicitudes;	
 	private double totalGenerado;
 	
 	
-	
+
+	public ServicioEstadisticaDTO() {}
+
+	public ServicioEstadisticaDTO(String nombreServicio, int cantidadSolicitudes, double totalGenerado) {
+		this.nombreServicio = nombreServicio;
+		this.cantidadSolicitudes = cantidadSolicitudes;
+		this.totalGenerado = totalGenerado;
+	}
+
+	public String getNombreServicio() {
+		return nombreServicio;
+	}
+
+	public void setNombreServicio(String nombreServicio) {
+		this.nombreServicio = nombreServicio;
+	}
+
+	public int getCantidadSolicitudes() {
+		return cantidadSolicitudes;
+	}
+
+	public void setCantidadSolicitudes(int cantidadSolicitudes) {
+		this.cantidadSolicitudes = cantidadSolicitudes;
+	}
+
+	public double getTotalGenerado() {
+		return totalGenerado;
+	}
+
+	public void setTotalGenerado(double totalGenerado) {
+		this.totalGenerado = totalGenerado;
+	}
+
 }
