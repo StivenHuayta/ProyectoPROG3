55f6956aaa5c17539e25eb3f6e104fd70b7c2278
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-14 12:34:47 -0500

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,9 @@
+public class Persona{
+	private String dni;
+	private String nombres;
+	private String apellidoP; 
+	private String apellidoM; 
+	private String telefono; 
+	private String email; 
+	
+}
