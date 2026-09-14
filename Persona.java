2db41a4474c0da5c637bb3043641786859cd2d9e
AlParSil <alvaroparedessilguera@gmail.com>
2026-09-14 12:36:44 -0500

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,9 +1,68 @@
-public class Persona{
+public abstract class Persona{
 	private String dni;
 	private String nombres;
 	private String apellidoP; 
 	private String apellidoM; 
 	private String telefono; 
 	private String email; 
+
+	 public Persona(String dni, String nombres, String apellidoP, String apellidoM, String telefono, String email) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 	
 }
