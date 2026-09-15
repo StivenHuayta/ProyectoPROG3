8ce3b44a10c408fe8863240669a909cb09ca781c
GonzaloCar574 <a20241911@pucp.edu.pe>
2026-09-15 11:15:30 -0500

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -71,60 +71,37 @@ public boolean esVeterinario() {
 
     //----------------------------------------------------------------------
 
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public int getid(){
+        return id; 
+    
     }
 
     public Persona getDatosPersonales() { 
         return datosPersonales; 
     } 
-
-    public void setDatosPersonales(Persona datosPersonales) {
-        this.datosPersonales = datosPersonales;
-    }
-
-    public String getNumeroColegiatura() {
-        return numeroColegiatura;
-    }
-
-    public void setNumeroColegiatura(String numeroColegiatura) {
-        this.numeroColegiatura = numeroColegiatura;
-    }
     
     public Usuario getCredenciales() { 
         return credenciales; 
     }
-
-    public void setCredenciales(Usuario credenciales) {
-        this.credenciales = credenciales;
+    
+    
+    
+    public String getNumeroColegiatura() {
+        return numeroColegiatura;
     }
+
 
     public boolean isActivo() {
         return activo;
     }
 
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-
+   
     public List<RolEmpleado> getRoles() {
         return roles;
     }
 
-    public void setRoles(List<RolEmpleado> roles) {
-        this.roles = roles;
-    }
-
     public Sede getSede() {
         return sede;
-    }
-
-    public void setSede(Sede sede) {
-        this.sede = sede;
     }
 
 }
